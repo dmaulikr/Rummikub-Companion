dc9215e5961aadb4e55e8b96cdb598aa52225ff9
Esdras Emanuel Vital Teixeira <esdrasevt@gmail.com>
2017-07-08 11:25:48 -0500

    Updated Multiple Items Appearence

--- Rummikub Companion/CustomBarBtnItem.swift
+++ Rummikub Companion/CustomBarBtnItem.swift
@@ -12,8 +12,7 @@ class CustomBarBtnItem: UIBarButtonItem {
     
     override func awakeFromNib() {
         
-        let controlState :UIControlState = .normal
-        self.setTitleTextAttributes([NSFontAttributeName: UIFont(name: "Helvetica Neue", size: 17)!, NSForegroundColorAttributeName: UIColor.white], for: controlState)
+        self.setTitleTextAttributes([NSFontAttributeName: UIFont(name: "Helvetica Neue", size: 17)!, NSForegroundColorAttributeName: UIColor.white], for: .normal)
        
     }
     
